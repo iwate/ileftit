8cd0b239fd0539c922869f34ba324829a9fa7ebc
iwate <github@iwate.me>
2023-09-16 12:03:30 +0900

    fix lint

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -10,7 +10,8 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
   const clientId = process.env.AZURE_AD_B2C_CLIENT_ID;
   const tenantName = process.env.AZURE_AD_B2C_TENANT_NAME;
   let userFlow = process.env.AZURE_AD_B2C_PRIMARY_USER_FLOW; // flow used by default
-  const b2cPasswordResetFlow = process.env.AZURE_AD_B2C_PASSWORD_RESET_USER_FLOW;
+  const b2cPasswordResetFlow =
+    process.env.AZURE_AD_B2C_PASSWORD_RESET_USER_FLOW;
 
   // #region Handle Azure B2C callbacks
 
@@ -20,7 +21,10 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
 
   console.log('Hello');
   console.log(req.method, req.url, req.body);
-  if (req.method === 'GET' && req.url.startsWith(`/api/auth/callback/${b2cProviderName}/`)) {
+  if (
+    req.method === 'GET' &&
+    req.url.startsWith(`/api/auth/callback/${b2cProviderName}/`)
+  ) {
     console.log(`Azure B2C callback:`, req.method, req.url, {
       query: req.query,
       body: req.body,
@@ -45,13 +49,16 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
           .replace(/\n/g, '')
           .replace(/\r\n/g, '');
 
-      console.log('Redirecting to B2C password reset flow:', b2cPasswordResetFlowUrl);
+      console.log(
+        'Redirecting to B2C password reset flow:',
+        b2cPasswordResetFlowUrl
+      );
       return res.redirect(b2cPasswordResetFlowUrl);
     }
     // handle cancel clicked during password reset
     else if (
       req.query?.error === 'access_denied' &&
-      req.query?.error_description?.indexOf(b2cPasswordResetCancelErrorCode) !== -1
+      req.query?.error_description?.includes(b2cPasswordResetCancelErrorCode)
     ) {
       console.log(
         'Detected Azure B2C password reset cancel error callback. Code:',
@@ -90,31 +97,4 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
   };
 
   return NextAuth(req, res, options);
-}
-
-// import NextAuth from 'next-auth';
-// export const authOptions = {
-//   providers: [
-//     AADB2CProvider({
-//       tenantId: process.env.AZURE_AD_B2C_TENANT_NAME,
-//       clientId: process.env.AZURE_AD_B2C_CLIENT_ID,
-//       clientSecret: process.env.AZURE_AD_B2C_CLIENT_SECRET,
-//       primaryUserFlow: process.env.AZURE_AD_B2C_PRIMARY_USER_FLOW,
-//       authorization: { params: { scope: 'offline_access openid' } },
-//     }),
-//   ],
-//   callbacks: {
-//     async jwt({ token, account }) {
-//       if (account) {
-//         token.uid = account.providerAccountId.replaceAll('-', '');
-//         token.idToken = account.id_token;
-//       }
-//       return token;
-//     },
-//     async session({ session, token, user }) {
-//       session.user.uid = token.uid;
-//       return session;
-//     },
-//   },
-// };
-// // export default NextAuth(authOptions);
+};
